0ff87f43cfcdc4cf4c51f9fd0c3b8dddbe16bfa6
itachi killer <sanjanikumarsingh@gmail.com>
2020-08-03 23:47:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+var typed=new Typed('#typed',{
+    strings:[
+        'web developer','freelancer'], 
+        typeSpeed:50,backSpeed:50,
+    loop:true
+});
+var typed_2=new Typed('#typed_2',{
+    strings:[
+        'web developer',
+        'freelancer'
+    ], 
+        typeSpeed:50,backSpeed:50,
+    loop:true
+});
